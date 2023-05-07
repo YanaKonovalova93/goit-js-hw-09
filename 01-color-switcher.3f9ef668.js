267c0fe2d563f62d88926483d32816401c25219a
YanaKonovalova93 <118968627+YanaKonovalova93@users.noreply.github.com>
2023-05-07 20:26:52 +0000

    Deploying to gh-pages from @ YanaKonovalova93/goit-js-hw-09@4b153a1913439a73502759a22d8a5cb807c80fc8 🚀

---
+++ 01-color-switcher.3f9ef668.js
@@ -0,0 +1,2 @@
+const t=document.body,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");let o=null;e.addEventListener("click",(function(){o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.setAttribute("disabled","disabled")})),n.addEventListener("click",(function(){clearInterval(o),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.3f9ef668.js.map
