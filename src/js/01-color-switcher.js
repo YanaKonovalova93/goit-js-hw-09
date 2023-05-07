4b153a1913439a73502759a22d8a5cb807c80fc8
YanaKonovalova93 <mikobacteria@gmail.com>
2023-05-07 23:26:11 +0300

    done 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const bodyEl = document.body;
+const buttonStartEl = document.querySelector('button[data-start]');
+const buttonStopEl = document.querySelector('button[data-stop]');
 
+let timerId = null;
+
+buttonStartEl.addEventListener('click', onChangeColor);
+buttonStopEl.addEventListener('click', stopChangeColor);
+
+function onChangeColor() {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStartEl.setAttribute('disabled', 'disabled');
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  buttonStartEl.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
